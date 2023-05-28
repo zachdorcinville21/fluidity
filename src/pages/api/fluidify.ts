5bbd8da7cddbaedfa5c313bbeb2d39e5153b45a5
Zachary Dorcinville <zdorcinville@gmail.com>
2023-05-28 15:01:59 -0400

    add env var for organization

--- src/pages/api/fluidify.ts
+++ src/pages/api/fluidify.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Configuration, OpenAIApi } from 'openai';
 
 const config = new Configuration({
-  organization: 'org-euw0gFS8ZCrJFWaza9D2BNUG',
+  organization: process.env.OPEN_AI_ORG,
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
